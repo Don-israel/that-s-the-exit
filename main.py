37ab4a945100a50d0b68e8dc7c8072f4f2cc14d2
Don_israel <israelojakorotu@gmail.com>
2024-12-12 12:55:37 +0100

    thats the exit

---
+++ main.py
@@ -0,0 +1,13 @@
+# python program to demonstrate
+# exit()
+
+for i in range(10):
+    # If the value of i become
+    # 5 then the program is forced
+    #to exit
+    if i == 5:
+
+        # prints the exit message
+        print(exit)
+        exit()
+    print(i)
